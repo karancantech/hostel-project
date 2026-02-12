1d8501db66ce9c5c1174bf283965102338b4c90e
karancantech <karan.cantech@gmail.com>
2026-02-12 12:16:10 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,12 @@
 <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="hostel";
-$mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+$dbuser = "root";
+$dbpass = "H7g1loZk7jOKiC1hi2l8wErLuHgiOVstUXz9MZBkMKwCdPAsFJjkT3lbFZS408FH";   // <-- put correct password here
+$host   = "mysql";                      // <-- change from localhost
+$db     = "hostel";
+
+$mysqli = new mysqli($host, $dbuser, $dbpass, $db);
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
 ?>
