72ff4239420dbf0fdb8eb8c7bacdb085672e7bf3
karancantech <karan.cantech@gmail.com>
2026-02-12 12:28:25 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 $dbuser = "mysql";
 $dbpass = "fYtnhTxyk2uoNurJU00hCCiOzqfNjDPHcyJaAKbsB9INqSjd9YwQhdEJUleb9kEr";
-$host = "mysql-database-mc0wco404s4wwck44osgkwko";  // MySQL container name
+$host = "mc0wco404s4wwck44osgkwko";  // MySQL container name
 $db = "hostel";
 
 $mysqli = new mysqli($host, $dbuser, $dbpass, $db);
