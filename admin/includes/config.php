b7937b7e74147d454a46ba69e4c00cf8c84562b0
karancantech <karan.cantech@gmail.com>
2026-02-12 12:24:54 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,7 @@
 <?php
 $dbuser = "root";
 $dbpass = "H7g1loZk7jOKiC1hi2l8wErLuHgiOVstUXz9MZBkMKwCdPAsFJjkT3lbFZS408FH";   // <-- put correct password here
-$host   = "mysql";                      // <-- change from localhost
+$host   = "mysql-database-mc0wco404s4wwck44osgkwko";                      // <-- change from localhost
 $db     = "hostel";
 
 $mysqli = new mysqli($host, $dbuser, $dbpass, $db);
