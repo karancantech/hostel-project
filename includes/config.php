6d67bfdedd66635ef6a0af187a6e91e0f0e213ef
karancantech <karan.cantech@gmail.com>
2026-02-13 12:21:08 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-$dbuser = getenv('DB_USER');
-$dbpass = getenv('DB_PASSWORD');
-$host   = getenv('DB_HOST');
-$db     = getenv('DB_DATABASE');
+$host = "mc0wco404s4wwck44osgkwko";   // from MySQL URL
+$dbuser = "mysql";                  // Normal User from Coolify
+$dbpass = "fYtnhTxyk2uoNurJU00hCCiOzqfNjDPHcyJaAKbsB9INqSjd9YwQhdEJUleb9kEr";     // Normal User Password
+$db = "default";                    // Initial database name
 
 $mysqli = new mysqli($host, $dbuser, $dbpass, $db);
 
